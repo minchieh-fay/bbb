4b4ce91b751b8436f669d9ebfecee5e64f100674
minchieh <minchieh@hotmail.com>
2025-08-30 17:37:25 +0800

    123123

--- app.go
+++ app.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/adshao/go-binance/v2"
@@ -17,6 +19,23 @@ type App struct {
 // NewApp creates a new App application struct
 func NewApp() *App {
 	if fc == nil {
+		// 从环境变量获取代理设置
+		proxyEnv := os.Getenv("baproxy")
+		if proxyEnv != "" {
+			// 如果设置了代理环境变量，则使用代理
+			proxyURL, err := url.Parse(proxyEnv)
+			if err == nil {
+				http.DefaultTransport = &http.Transport{
+					Proxy: http.ProxyURL(proxyURL),
+				}
+				fmt.Printf("使用代理: %s\n", proxyEnv)
+			} else {
+				fmt.Printf("代理地址解析失败: %s, 错误: %v\n", proxyEnv, err)
+			}
+		} else {
+			fmt.Println("未设置代理，使用直连")
+		}
+
 		fc = binance.NewFuturesClient(os.Getenv("BINANCE_API_KEY"), os.Getenv("BINANCE_SECRET_KEY"))
 	}
 	return &App{}
